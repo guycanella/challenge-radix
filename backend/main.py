81aa4b38e466e489f90b629dcd59840a547ec05d
Guilherme <guycanella@gmail.com>
2024-09-07 09:32:02 -0300

    create routes

--- backend/main.py
+++ backend/main.py
@@ -3,5 +3,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def home():
+    return { "message": "Hello, World" }
+
+@app.get("/data-sensor")
+def get_data_sensor():
+    return {}
+
+@app.post("/data-sensor")
+def post_data_sensor():
+    return {}
+
+@app.post("/data-sensor-csv")
+def post_data_sensor_csv():
+    return {}
